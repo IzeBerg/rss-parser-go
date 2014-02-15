143353ed11bf37df93912c0d07d034fb9522cb2a
Joseph Kiok <joseph@kiok.org>
2014-02-15 15:04:28 -0800

    Added logging.  And fixed the case when errors doesn't exist, to return os.ErrNotExist (instead of nil).

--- rssparser.go
+++ rssparser.go
@@ -9,26 +9,30 @@ package rss
 import (
 	"encoding/xml"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 func ParseRSS(url string) (*RSS, error) {
 
-	rss := RSS{}
 	response, err1 := http.Get(url)
 	if err1 != nil {
+		log.Println("Unable to retrieve via HTTP", url)
 		return nil, err1
 	}
 	defer response.Body.Close()
 
 	data, err2 := ioutil.ReadAll(response.Body)
 	if err2 != nil {
+		log.Println("Unable to read bytes from socket")
 		return nil, err2
 	}
 
+	rss := RSS{}
 	err3 := xml.Unmarshal(data, &rss)
 	if err3 != nil {
 		return nil, err3
 	}
-	return &rss, nil
+	return &rss, os.ErrNotExist
 }
--- rssparser_test.go
+++ rssparser_test.go
@@ -1,11 +1,16 @@
 package rss
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestFetchRss(t *testing.T) {
-	ParseRSS("http://www.rssboard.org/files/sample-rss-091.xml")
-	ParseRSS("http://www.rssboard.org/files/sample-rss-092.xml")
-	ParseRSS("http://www.rssboard.org/files/sample-rss-2.xml")
+	//ParseRSS("http://www.rssboard.org/files/sample-rss-091.xml")
+	//ParseRSS("http://www.rssboard.org/files/sample-rss-092.xml")
+	rssObject, error := ParseRSS("http://www.rssboard.org/files/sample-rss-2.xml")
+
+	if error != nil {
+		fmt.Println(rssObject)
+	}
 }
